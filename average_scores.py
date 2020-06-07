15f84ac9cc305ab36b6e43d7c893802ed63f923b
Abutalibhasan <66400651+Abutalibhasan@users.noreply.github.com>
2020-06-07 16:51:49 -0400

    Add files via upload

---
+++ average_scores.py
@@ -0,0 +1,18 @@
+def average():
+  fname = input("Enter your first name:")
+  lname = input("Enter your last name:")
+  age = input("Enter your age:")
+  x = input("Enter score1")
+  y = input("Enter score2")
+  z = input("Enter score")
+  a = (int(x) + int(y) + int(z))
+  print(lname +","+ fname +" "+"age:"+ age +" "+"average grade:" )
+  print(a/3)
+if __name__ == '__main__':
+  average()
+
+
+
+
+
+
